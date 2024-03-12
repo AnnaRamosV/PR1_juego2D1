663016d71d6173806a84c86ef845af1e03b319df
AnnaRamosV <aramval@upv.edu.es>
2024-03-12 14:21:09 +0100

    Movimiento Salto

--- Assets/Scripts/Movpersonaje.cs
+++ Assets/Scripts/Movpersonaje.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movpersonaje : MonoBehaviour
-{
+{ 
+     //public float multVelocidad = 1;
+    //public float multSalto = 1;
+
+    //RigidBody2d rb;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
-    }
+         rb = this.GetComponent<RigidBody2d> ();
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Movimiento
+        float mov = Input.GetAxis("Horizontal")*multVelocidad*Time.deltaTime;
+        transform.position = new Vector3(transform.position.x+mov, transform.position.y, transform.position.z);
+        transform.Translate(mov, 0, 0);
+
+        //Salto
+         if (Input.GetKeyDown(KeyCode.Space)) {
+         rb.AddForce(new Vector2(0, multSalto), ForceMode2d.Impulse);
+         }
     }
+
+   
 }
